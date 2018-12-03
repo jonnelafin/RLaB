d05535ead7dede07b39f22061e4719b1c1b1a7d6
Ryhmatunnus tktlinkki <tktlinkki@lx4-cs-link-03.cs.helsinki.fi>
2018-12-03 18:43:36 +0200

    final commit

--- Rolling(Like a ball)/Assets/Scripts/Objects/Player/Movement.cs
+++ Rolling(Like a ball)/Assets/Scripts/Objects/Player/Movement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 ///using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour {
+	public GameObject nextLVL;
 	public float speed;
 	public float score = 0F;
 	private Rigidbody rigidbody;
 	public float isSpr = 1;
+	public bool isFlying = false;
 	void Start(){
 		rigidbody = GetComponent<Rigidbody>();
 	}
@@ -20,15 +22,12 @@ public class Movement : MonoBehaviour {
         {
             isSpr = 1;
         }
-		if (rigidbody.position.y < -10F)
+		if (rigidbody.position.y < -2F)
 		{
 			print("You died!");
 			Application.LoadLevel(Application.loadedLevel);
 		}
-		if (score >= 8F){
-			print("You won!");
-			Application.LoadLevel(Application.loadedLevel);
-		}
+		
 	}
 	// Update is called once per frame
 	void FixedUpdate(){
@@ -37,12 +36,29 @@ public class Movement : MonoBehaviour {
 
 		Vector3 movement = new Vector3(velH, 0.0F, velV);
 		rigidbody.AddForce(movement*speed*isSpr);
+
+		if(Input.GetButtonDown("Fire1")){
+			if(isFlying == false){
+				isFlying = true;
+				Vector3 jump = new Vector3(0.0F, 200.0F, 0.0F);
+
+				rigidbody.AddForce(jump);
+			}
+		}
 	}
 	void OnTriggerEnter(Collider other)
     {
 		if(other.gameObject.CompareTag ("Collectable")){
         	Destroy(other.gameObject);
 			score = score + 1F;
+		}
+		if (score >= 8F){
+			print("You won!");
+//			Application.LoadLevel(Application.loadedLevel);
+			nextLVL.transform.position = nextLVL.transform.position + new Vector3(0F, 10, 0F);
+		}
+		if(other.gameObject.CompareTag ("Ground")){
+        	isFlying = false;
 		}
     }
 }
